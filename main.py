6dc54aefa6c31abdd717453cd2bc9240fd61aa3e
SofiaAristy1491 <ingsofiaaristizabal.visar@gmail.com>
2025-04-29 19:59:57 -0500

    29 de Abril

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from operaciones import sumar 
+from operaciones import resta
+from operaciones import multiplicacion
+from operaciones import division
+from operaciones import division2
+from livereload import Server
 
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return '''
-<h1> Aplicación de la Calculadora</h1>
-<a href="/suma?num1=5&num2=4"> Ir a la página de suma</a> 
+    return render_template("home.html")
+# '''
+# <h1> Aplicación de la Calculadora</h1>
+# <a href="/suma?num1=5&num2=4"> Ir a la página de suma</a> 
+# <a href="/resta?num1=5&num2=4"> Ir a la página de resta</a> 
+# <a href="/multiplicacion?num1=5&num2=4"> Ir a la página de multiplicacion</a> 
+# <a href="/division?num1=5&num2=4"> Ir a la página de division</a> 
+# <a href="/division piso?num1=5&num2=4"> Ir a la página de division piso</a> 
 
 
-
-
-'''
 @app.route("/suma")
 def ruta_suma():
     num1=request.args.get("num1",type=float)
@@ -21,3 +28,39 @@ def ruta_suma():
     if num1 is None or num2 is None: 
         return "faltan datos"
     return f"la suma num1 y num2 es {sumar(num1,num2)}"
+
+@app.route("/resta")
+def ruta_resta():
+    num1=request.args.get("num1",type=float)
+    num2=request.args.get("num2",type=float)
+    if num1 is None or num2 is None: 
+        return "faltan datos"
+    return f"la suma num1 y num2 es {resta(num1,num2)}"
+
+@app.route("/multiplicacion")
+def ruta_multiplicacion():
+    num1=request.args.get("num1",type=float)
+    num2=request.args.get("num2",type=float)
+    if num1 is None or num2 is None: 
+        return "faltan datos"
+    return f"la suma num1 y num2 es {multiplicacion(num1,num2)}"
+
+@app.route("/division")
+def ruta_division():
+    num1=request.args.get("num1",type=float)
+    num2=request.args.get("num2",type=float)
+    if num1 is None or num2 is None: 
+        return "faltan datos"
+    return f"la suma num1 y num2 es {division(num1,num2)}"
+
+@app.route("/division piso")
+def ruta_division2():
+    num1=request.args.get("num1",type=float)
+    num2=request.args.get("num2",type=float)
+    if num1 is None or num2 is None: 
+        return "faltan datos"
+    return f"la division piso num1 y num2 es {division2(num1,num2)}"
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.serve()
--- operaciones.py
+++ operaciones.py
@@ -1,2 +1,14 @@
 def sumar(a,b):
     return a+b
+
+def resta(c,d):
+    return c-d
+
+def multiplicacion(e,f):
+    return (e*f)
+
+def division(g,h):
+    return (g/h)
+
+def division2(g,h):
+    return (g//h)
